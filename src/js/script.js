b3d028942938dc856c0e8d6fab2423d9a460556a
Dmitry Ageev <dimichageev@gmail.com>
2021-06-26 14:47:26 +0300

    portfolio end

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
 	closeElem = document.querySelector('.menu__close'),
-	overlayElwm = document.querySelector('.menu__overlay');
+	overlayElwm = document.querySelector('.menu__overlay'),
+	counters = document.querySelectorAll('.skills__ratings-counter'),
+	lines = document.querySelectorAll('.skills__ratings-line span');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
@@ -13,4 +15,8 @@ closeElem.addEventListener('click', () => {
 
 overlayElwm.addEventListener('click', () => {
 	menu.classList.remove('active');
+});
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
